33dd35b47361592c6113f662122e67c922628cf1
rogerscj <rogerscj@rose-hulman.edu>
2018-12-11 21:02:59 -0500

    done

--- src/problem4.py
+++ src/problem4.py
@@ -3,8 +3,8 @@ Exam 1, problem 4.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Christina Rogers.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -68,13 +68,23 @@ def problem4(number_of_stairs, step_size, starting_point, window):
       :type window:            rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # IMPORTANT: For PARTIAL CREDIT, you can draw just the black "bottoms"
     #            of the stair steps.
     # -------------------------------------------------------------------------
-
-
+    starting_point.attach_to(window)
+    for k in range (number_of_stairs):
+        verticalLine = rg.Line(rg.Point(starting_point.x+(k*step_size), starting_point.y-k*step_size), rg.Point(starting_point.x+(k*step_size), (starting_point.y)-((1+k)*step_size)))
+        horizontalLine = rg.Line(rg.Point(verticalLine.end.x, verticalLine.end.y), rg.Point(verticalLine.end.x + step_size, verticalLine.end.y))
+        horizontalLine.thickness = 3
+        verticalLine.thickness = 3
+        verticalLine.color = 'magenta'
+        verticalLine.attach_to(window)
+        horizontalLine.attach_to(window)
+    endpoint = horizontalLine.end
+    endpoint.attach_to(window)
+    window.render()
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
